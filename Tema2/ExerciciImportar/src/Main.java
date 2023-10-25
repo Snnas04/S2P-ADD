8283a92b322ca33d4bcb74df7384d779aac2ddec
Marc <marcsans26@gmail.com>
2023-10-25 19:05:59 +0200

    nova activitat, exercici importar

---
+++ Tema2/ExerciciImportar/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void llegirEmpleats() {
+        // leer el archivo empleats.dat
+        // mostrar por pantalla los empleados
+        
+    }
+}
