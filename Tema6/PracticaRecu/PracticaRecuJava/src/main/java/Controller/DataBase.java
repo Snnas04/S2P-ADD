379af70c3ce200bf32eb319bfefbe75204048fc1
Marc <marcsans26@gmail.com>
2024-03-06 19:49:45 +0100

    Projecte java per peracticar

---
+++ Tema6/PracticaRecu/PracticaRecuJava/src/main/java/Controller/DataBase.java
@@ -0,0 +1,68 @@
+package Controller;
+
+import Data.Aspirant;
+import Utils.DBExeptions;
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.ArrayList;
+
+import static com.mongodb.client.model.Projections.include;
+
+public class DataBase {
+    private MongoClient client;
+    private MongoDatabase db;
+    private MongoCollection<Document> collection;
+
+    public DataBase(String host, int port, String dbName, String collectionName) {
+        client = MongoClients.create("mongodb://snnas04:1234@" + host + ":" + port);
+        db = client.getDatabase(dbName);
+        collection = db.getCollection(collectionName);
+    }
+
+    public void insert(Document doc) throws DBExeptions {
+        if (doc == null)
+            throw new DBExeptions("Document is null");
+        collection.insertOne(doc);
+    }
+
+    public ArrayList<Aspirant> getAspirants() {
+        ArrayList<Aspirant> aspirantsList = new ArrayList<>();
+
+        try (MongoCursor<Document> cursor = collection.find().iterator()) {
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                Aspirant aspirant = new Aspirant(doc.getString("nif"), doc.getString("nom"), doc.getString("llinatges"), doc.getString("codiPostal"));
+
+                aspirantsList.add(aspirant);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return aspirantsList;
+    }
+
+    public ArrayList<Document> findSkipLimit(int from, int quantity) {
+        return collection.find().skip(from - 1).limit(quantity).into(new ArrayList<>());
+    }
+
+    public ArrayList<Document> findByCP(String cp) {
+        Document filter = new Document("codiPostal", cp);
+        return collection.find(filter).into(new ArrayList<>());
+    }
+
+    public ArrayList<Document> findByCPName(String cp, String name) {
+        Document filter = new Document("codiPostal", cp).append("nom", name);
+
+        return collection.find(filter).into(new ArrayList<>());
+    }
+
+    public ArrayList<Document> findAllProjection(ArrayList<String> names) {
+        return collection.find().projection(include(names)).into(new ArrayList<>());
+    }
+
+    public ArrayList<Document> findAllSort(String field, boolean asc) {
+        return collection.find().sort(new Document(field, asc ? 1 : -1)).into(new ArrayList<>());
+    }
+}
---
+++ Tema6/PracticaRecu/PracticaRecuJava/src/main/java/Main.java
@@ -0,0 +1,61 @@
+import Controller.DataBase;
+import Data.Aspirant;
+import org.bson.Document;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBase db = new DataBase("localhost", 27017, "TestDB", "aspirants");
+
+        // Create an Aspirant object
+//        Aspirant aspirant = new Aspirant("12345678A", "Marc", "Sans", "07140");
+//        Document doc = new Document("nif", aspirant.getNif())
+//                .append("nom", aspirant.getName())
+//                .append("llinatges", aspirant.getSurname())
+//                .append("codiPostal", aspirant.getCp());
+//
+//        try {
+//            db.insert(doc);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        // Create another Aspirant object with toMap() method
+//        Aspirant aspirant2 = new Aspirant("87654321B", "Pau", "Sans", "07140");
+//        Document doc2 = new Document(aspirant2.toMap());
+//
+//        try {
+//            db.insert(doc2);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        // 7
+        db.getAspirants().forEach(System.out::println);
+
+        // 11
+//        db.findSkipLimit(10, 5).forEach(System.out::println);
+
+        // 12
+//        db.findByCP("07300").forEach(System.out::println);
+
+        // 13
+//        db.findByCPName("07421", "Margalida").forEach(System.out::println);
+
+        // 14
+//        ArrayList<String> camps = new ArrayList<>();
+//        camps.add("nif");
+//        camps.add("nom");
+//        camps.add("llinatges");
+//        camps.add("codiPostal");
+//
+//        db.findAllProjection(camps).forEach(System.out::println);
+
+        // 15
+//        db.findAllSort("nom", true).forEach(System.out::println);
+
+        // 16
+
+    }
+}
